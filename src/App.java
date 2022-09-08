60d3af44a5a5d0b1dee9d815307ca2fa935cd9e8
João Victor de Andrade <joao.victor@primesquad.com.br>
2022-09-07 23:29:25 -0300

    feat: Create app flow

--- src/App.java
+++ src/App.java
@@ -2,47 +2,62 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        /* 
-            Node<Integer> firstNode = new Node<Integer>();
-            firstNode.setValue(10);
-
-            Node<Integer> secondNode = new Node<Integer>(16, firstNode);
-
-            Node<Integer> thirdNode = new Node<Integer>(97, firstNode);
-
-            Node<Integer> forthNode = new Node<Integer>(169, firstNode);
-
-            Node<Integer> fiftyNode = new Node<>(666, secondNode);
-
-            firstNode.insertNodeToBranch(secondNode);
-            firstNode.insertNodeToBranch(thirdNode);
-            firstNode.insertNodeToBranch(forthNode);
-
-            secondNode.insertNodeToBranch(fiftyNode);
-
-            BTreePrinter.printNode(firstNode);
-        */
         Scanner scan = new Scanner(System.in);
-        
-
         System.out.print("enter the numbers of nodes: ");
         Integer NumberNodes = scan.nextInt();
-        
 
         Node<Integer> root = new Node<Integer>();
 
         for (int i = 0; i < NumberNodes; i++) {
             System.out.print("enter value of the node: ");
-            Integer nodes = scan.nextInt();
+            int value = scan.nextInt();
 
             if (i == 0) {
-                root.setValue(nodes);
+                root.setValue(value);
             } else {
-                Node<Integer> node = new Node<Integer>(nodes, root);
-                root.insertNodeToBranch(node);
+                if (root.getLeft() != null && root.getCenter() != null && root.getRight() != null) {
+                       for (int j = i; j < NumberNodes; j++){
+                               boolean firstExec = true;
+                               int branchNode = 0;
+
+                               if (j != i) {
+                                   firstExec = false;
+                               }
+
+                               if (firstExec) {
+                                       System.out.println("In which node do you want to insert this value to branch? : ");
+                                       branchNode = scan.nextInt();
+                               }
+
+                               if (!firstExec) {
+                                       System.out.print("enter value of the node: ");
+                                       value = scan.nextInt();
+                                       System.out.println("In which node do you want to insert this value to branch? : ");
+                                       branchNode = scan.nextInt();
+                               }
+
+
+                               var foundNode = root.findElement(root, branchNode);
+
+                               if (foundNode != null) {
+                                       var newNode = new Node<>(value, foundNode);
+                                       foundNode.insertNodeToBranch(newNode);
+
+                                       BTreePrinter.printNode(root);
+                               }
+                       }
+                } else {
+
+                    Node<Integer> node = new Node<Integer>(value, root);
+                    root.insertNodeToBranch(node);
+                    BTreePrinter.printNode(root);
+                }
+
            }
         }
+
+
+        root.transformToBinary(root);
 
         BTreePrinter.printNode(root);
         scan.close();
